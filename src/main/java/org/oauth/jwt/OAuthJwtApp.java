591f1dc6be0f9f603b2616cebe59e8a57839d3c0
303244 <303244@LTIN136132.cts.com>
2022-01-14 18:20:23 +0530

    JWT Service

--- src/main/java/org/oauth/jwt/OAuthJwtApp.java
+++ src/main/java/org/oauth/jwt/OAuthJwtApp.java
@@ -2,10 +2,12 @@ package org.oauth.jwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @CrossOrigin
+//@EnableEurekaClient
 public class OAuthJwtApp {
 	
 	public static void main(String[] args) {
